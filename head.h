29aa13c9d5f6a608a3698f9f2bad4f4482f05ccf
heart1016 <heart10162115@foxmail.com>
2015-08-28 08:54:26 +0800

    heard.h

---
+++ head.h
@@ -0,0 +1,27 @@
+/*************************************************************************
+    > File Name: head.h
+  > Author:Hack 
+  > Mail:heart10162115@foxmail.com 
+  > Created Time: 2015年07月09日 星期四 10时30分59秒
+ ************************************************************************/
+
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<strings.h>
+#include<errno.h>
+#include<ctype.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/stat.h>
+#include<arpa/inet.h>
+
+#define SERV_PORT 8000
+#define SERV_IP "192.168.42.39"
+
+#endif
